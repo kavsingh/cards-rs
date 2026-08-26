4c928de962b02c036e2ee7e2024b68f979e43d17
Kav Singh <flakisms@gmail.com>
2026-08-26 03:49:07 +0200

    wip: hand enum

---
+++ poker-cards/src/hand2/compare.rs
@@ -0,0 +1,268 @@
+use super::{Hand, HighCard, Pair, Straight, ThreeOfAKind, TwoPair};
+use crate::util::cmp_max;
+use crate::{Card, Rank};
+
+impl Ord for HighCard {
+	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+		self.high_card
+			.cmp(&other.high_card)
+			.then_with(|| cmp_max(&self.kickers, &other.kickers))
+	}
+}
+
+impl PartialOrd for HighCard {
+	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+		Some(self.cmp(other))
+	}
+}
+
+impl PartialEq for HighCard {
+	fn eq(&self, other: &Self) -> bool {
+		self.cmp(other).is_eq()
+	}
+}
+
+impl Eq for HighCard {}
+
+//
+
+impl Ord for Pair {
+	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+		cmp_max(&self.pair, &other.pair)
+			.then_with(|| cmp_max(&self.kickers, &other.kickers))
+	}
+}
+
+impl PartialOrd for Pair {
+	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+		Some(self.cmp(other))
+	}
+}
+
+impl PartialEq for Pair {
+	fn eq(&self, other: &Self) -> bool {
+		self.cmp(other).is_eq()
+	}
+}
+
+impl Eq for Pair {}
+
+//
+
+impl Ord for TwoPair {
+	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+		cmp_max(&self.high_pair, &other.high_pair)
+			.then_with(|| cmp_max(&self.low_pair, &other.low_pair))
+			.then_with(|| cmp_max(&self.kickers, &other.kickers))
+	}
+}
+
+impl PartialOrd for TwoPair {
+	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+		Some(self.cmp(other))
+	}
+}
+
+impl PartialEq for TwoPair {
+	fn eq(&self, other: &Self) -> bool {
+		self.cmp(other).is_eq()
+	}
+}
+
+impl Eq for TwoPair {}
+
+//
+
+impl Ord for ThreeOfAKind {
+	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+		cmp_max(&self.triplet, &other.triplet)
+			.then_with(|| cmp_max(&self.kickers, &other.kickers))
+	}
+}
+
+impl PartialOrd for ThreeOfAKind {
+	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+		Some(self.cmp(other))
+	}
+}
+
+impl PartialEq for ThreeOfAKind {
+	fn eq(&self, other: &Self) -> bool {
+		self.cmp(other).is_eq()
+	}
+}
+
+impl Eq for ThreeOfAKind {}
+
+//
+
+fn cmp_straight(a: &[Card; 5], b: &[Card; 5]) -> std::cmp::Ordering {
+	match (a[4].rank == Rank::Ace, b[4].rank == Rank::Ace) {
+		(true, false) => std::cmp::Ordering::Less,
+		(false, true) => std::cmp::Ordering::Greater,
+		_ => cmp_max(a, b),
+	}
+}
+
+//
+
+impl Ord for Straight {
+	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+		cmp_straight(&self.straight, &other.straight)
+	}
+}
+
+impl PartialOrd for Straight {
+	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+		Some(self.cmp(other))
+	}
+}
+
+impl PartialEq for Straight {
+	fn eq(&self, other: &Self) -> bool {
+		self.cmp(other).is_eq()
+	}
+}
+
+impl Eq for Straight {}
+
+//
+
+trait RankedHand {
+	fn rank(&self) -> u8;
+}
+
+impl RankedHand for Hand {
+	fn rank(&self) -> u8 {
+		match self {
+			Self::HighCard(_) => 0,
+			Self::Pair(_) => 1,
+			Self::TwoPair(_) => 2,
+			Self::ThreeOfAKind(_) => 3,
+			Self::Straight(_) => 4,
+			Self::Flush(_) => 5,
+			Self::FullHouse(_) => 6,
+			Self::FourOfAKind(_) => 7,
+			Self::StraightFlush(_) => 8,
+			Self::RoyalFlush(_) => 9,
+		}
+	}
+}
+
+impl Ord for Hand {
+	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+		self.rank()
+			.cmp(&other.rank())
+			.then_with(|| match (self, other) {
+				(Self::HighCard(a), Self::HighCard(b)) => a.cmp(b),
+				(Self::Pair(a), Self::Pair(b)) => a.cmp(b),
+				(Self::TwoPair(a), Self::TwoPair(b)) => a.cmp(b),
+				(Self::ThreeOfAKind(a), Self::ThreeOfAKind(b)) => a.cmp(b),
+				(Self::Straight(a), Self::Straight(b)) => a.cmp(b),
+				_ => std::cmp::Ordering::Equal,
+			})
+	}
+}
+
+impl PartialOrd for Hand {
+	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+		Some(self.cmp(other))
+	}
+}
+
+impl PartialEq for Hand {
+	fn eq(&self, other: &Self) -> bool {
+		self.cmp(other).is_eq()
+	}
+}
+
+impl Eq for Hand {}
+
+//
+
+#[cfg(test)]
+#[allow(clippy::unwrap_used, clippy::panic)]
+mod tests {
+	use super::{Hand, HighCard, Straight, TwoPair};
+
+	#[test]
+	#[allow(clippy::too_many_lines)]
+	fn should_compare_hands() {
+		let high_king_10_kicker = Hand::HighCard(HighCard {
+			high_card: "Kd".parse().unwrap(),
+			kickers: vec!["10c".parse().unwrap()],
+		});
+
+		let high_king_6_kicker = Hand::HighCard(HighCard {
+			high_card: "Kc".parse().unwrap(),
+			kickers: vec!["6d".parse().unwrap()],
+		});
+
+		let two_pair_jack_sixes = Hand::TwoPair(TwoPair {
+			high_pair: ["Jd".parse().unwrap(), "Jc".parse().unwrap()],
+			low_pair: ["6d".parse().unwrap(), "6c".parse().unwrap()],
+			kickers: vec![],
+		});
+
+		let two_pair_jack_fives = Hand::TwoPair(TwoPair {
+			high_pair: ["Jd".parse().unwrap(), "Jc".parse().unwrap()],
+			low_pair: ["5d".parse().unwrap(), "5c".parse().unwrap()],
+			kickers: vec![],
+		});
+
+		let straight_no_ace = Hand::Straight(Straight {
+			straight: [
+				"6h".parse().unwrap(),
+				"5d".parse().unwrap(),
+				"4c".parse().unwrap(),
+				"3d".parse().unwrap(),
+				"2s".parse().unwrap(),
+			],
+		});
+
+		let straight_ace_low = Hand::Straight(Straight {
+			straight: [
+				"5d".parse().unwrap(),
+				"4c".parse().unwrap(),
+				"3d".parse().unwrap(),
+				"2h".parse().unwrap(),
+				"Ad".parse().unwrap(),
+			],
+		});
+
+		let straight_ace_high = Hand::Straight(Straight {
+			straight: [
+				"Ad".parse().unwrap(),
+				"Kc".parse().unwrap(),
+				"Qh".parse().unwrap(),
+				"Jd".parse().unwrap(),
+				"10s".parse().unwrap(),
+			],
+		});
+
+		let mut hands = [
+			&straight_ace_high,
+			&two_pair_jack_sixes,
+			&straight_ace_low,
+			&high_king_10_kicker,
+			&two_pair_jack_fives,
+			&straight_no_ace,
+			&high_king_6_kicker,
+		];
+
+		hands.sort();
+
+		assert_eq!(
+			[
+				&high_king_6_kicker,
+				&high_king_10_kicker,
+				&two_pair_jack_fives,
+				&two_pair_jack_sixes,
+				&straight_ace_low,
+				&straight_no_ace,
+				&straight_ace_high,
+			],
+			hands,
+		);
+	}
+}
---
+++ poker-cards/src/hand2/mod.rs
@@ -0,0 +1,90 @@
+mod compare;
+mod extract;
+
+use crate::Card;
+
+pub struct HandCandidate {
+	sorted_cards: Vec<Card>,
+}
+
+impl HandCandidate {
+	#[must_use]
+	pub fn new(pocket_cards: &[Card; 2], community_cards: &[Card]) -> Self {
+		let mut sorted_cards = pocket_cards.to_vec();
+		let mut community = community_cards.to_owned();
+
+		sorted_cards.append(&mut community);
+		sorted_cards.sort_by(|a, b| b.cmp(a));
+
+		Self { sorted_cards }
+	}
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct HighCard {
+	pub high_card: Card,
+	pub kickers: Vec<Card>,
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct Pair {
+	pub pair: [Card; 2],
+	pub kickers: Vec<Card>,
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct TwoPair {
+	pub high_pair: [Card; 2],
+	pub low_pair: [Card; 2],
+	pub kickers: Vec<Card>,
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct ThreeOfAKind {
+	pub triplet: [Card; 3],
+	pub kickers: Vec<Card>,
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct Straight {
+	pub straight: [Card; 5],
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct Flush {
+	pub flush: [Card; 5],
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct FullHouse {
+	pub triplet: [Card; 3],
+	pub pair: [Card; 2],
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct FourOfAKind {
+	pub quad: [Card; 4],
+	pub kickers: Vec<Card>,
+}
+
+#[derive(Debug)]
+pub enum Hand {
+	HighCard(HighCard),
+	Pair(Pair),
+	TwoPair(TwoPair),
+	ThreeOfAKind(ThreeOfAKind),
+	Straight(Straight),
+	Flush(Flush),
+	FullHouse(FullHouse),
+	FourOfAKind(FourOfAKind),
+	StraightFlush(Straight),
+	RoyalFlush(Straight),
+}
